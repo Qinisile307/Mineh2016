6ed60b141b0687ec1e087854cf7abddb0af31ed1
Qinisile307 <thandomiya5@gmail.com>
2025-06-24 21:40:42 +0200

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# Tuples
+
+my_tuple = (1, 2, 3, 4, 5)
+
+print(my_tuple)
+print(my_tuple[0])  
+print(my_tuple[2])
+print(my_tuple[-1])
+
+tuplet1 = (1, 2, 3)
+tuplet2 = (4, 5, 6)
+
+conc_tuple = tuplet1 + tuplet2
+rep_tuple = tuplet1 * 3
+
+print(rep_tuple)
